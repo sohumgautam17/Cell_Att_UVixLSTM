67856cc6e3f57bffa9cc702414132baf16786d7b
willxxy <90741489+willxxy@users.noreply.github.com>
2024-07-20 11:12:30 -0400

    Update runners.py

--- runners.py
+++ runners.py
@@ -7,18 +7,24 @@ import numpy as np
 
 ## Functionized Training, Val, and Test Loops
 
-def trainer(model, train_loader, optimizer, device, args, loss):
+def trainer(model, train_loader, optimizer, device, args, dc_loss, bce_loss):
     model.train()
     losses = 0
     len_of_batch = 0
     
     for batch in tqdm(train_loader, desc = 'Training'):
         optimizer.zero_grad()
-        img, mask = batch
+        img, mask, _ = batch
         img, mask = img.to(device), mask.to(device)
-
+        # print(img.shape)
+        # input()
         output = model(img)
-        loss_value = loss(output, mask)
+        if args.loss == 'dice':
+            loss_value = dc_loss(output, mask)
+        elif args.loss == 'bce':
+            loss_value = bce_loss(output, mask)
+        elif args.loss == 'all':
+            loss_value = dc_loss(output, mask) + bce_loss(output, mask)
         loss_value.backward()
         optimizer.step_and_update_lr()
         losses += loss_value.item()
@@ -32,18 +38,23 @@ def trainer(model, train_loader, optimizer, device, args, loss):
     
     return average_loss
 
-def validater(model, val_loader, device, args, loss):
+def validater(model, val_loader, device, args, dc_loss, bce_loss):
     model.eval()
     losses = 0
     len_of_batch = 0
 
     with torch.no_grad():
         for batch in tqdm(val_loader, desc = 'Vaidating'):
-            img, mask = batch
+            img, mask, _ = batch
             img, mask = img.to(device), mask.to(device)
 
             output = model(img)
-            loss_value = loss(output, mask)
+            if args.loss == 'dice':
+                loss_value = dc_loss(output, mask)
+            elif args.loss == 'bce':
+                loss_value = bce_loss(output, mask)
+            elif args.loss == 'all':
+                loss_value = dc_loss(output, mask) + bce_loss(output, mask)
             losses += loss_value.item()
             len_of_batch += 1
             
@@ -93,12 +104,12 @@ def tester(model, test_loader, device, args):
 
     with torch.no_grad():
         for batch in tqdm(test_loader, desc='Testing'):
-            img, mask = batch
+            img, mask, _ = batch
             img, mask = img.to(device), mask.to(device)
             output = model(img)
             
             output = torch.sigmoid(output)  # Assuming sigmoid for binary classification
-            output = (output > 0.5).float()
+            output = (output > 0.5).float() # True is > 0.5 else false -> True -> 1.0 False -> 0.0
 
             dice = dice_coefficient(output, mask)
             iou_score = iou(output, mask)
